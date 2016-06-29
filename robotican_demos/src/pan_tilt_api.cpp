5a677f2fe4eb82fe21325f5c948451d860f6dc8a
tom wiss <tom@robotican.net>
2016-06-29 21:47:31 +0300

    update

--- robotican_demos/src/pan_tilt_api.cpp
+++ robotican_demos/src/pan_tilt_api.cpp
@@ -15,10 +15,11 @@ int main(int argc, char **argv) {
     std_msgs::Float64MultiArray multiArray;
     multiArray.data.push_back(0.0); // pan
     multiArray.data.push_back(0.0); // tilt
-
+    ros::Rate loopRate(1);
     while(ros::ok()) {
         pan_tilt_pub.publish(multiArray);
         ros::spinOnce();
+        loopRate.sleep();
     }
 
 
