2a384fb4e329384e1cc9fbd4315a0da8629205f2
tom wiss <tom@robotican.net>
2016-06-30 09:11:24 +0300

    update

--- robotican_demos/src/pan_tilt_api.cpp
+++ robotican_demos/src/pan_tilt_api.cpp
@@ -13,15 +13,11 @@ int main(int argc, char **argv) {
     ros::Publisher pan_tilt_pub = nodeHandle.advertise<std_msgs::Float64MultiArray>("pan_tilt_controller/command", 10);
 
     std_msgs::Float64MultiArray multiArray;
-<<<<<<< HEAD
     multiArray.data.push_back(0.2); // pan
     multiArray.data.push_back(0.3); // tilt (positive is down)
-
-=======
     multiArray.data.push_back(0.0); // pan
     multiArray.data.push_back(0.0); // tilt
     ros::Rate loopRate(1);
->>>>>>> 5a677f2fe4eb82fe21325f5c948451d860f6dc8a
     while(ros::ok()) {
         pan_tilt_pub.publish(multiArray);
         ros::spinOnce();
