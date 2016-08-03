b5f44be7a5dd4415e926721a7e330c505b3df5f8
Tom wiss <tom@robotican.net>
2016-08-03 08:20:01 +0300

    change f200 install

--- robotican/installations/third_pkg_setup/f200.sh
+++ robotican/installations/third_pkg_setup/f200.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-source ~/catkin_ws/devel/setup.bash
 cd ~
-git clone https://github.com/richardw347/RealSense-ROS.git
-cd ~/RealSense-ROS/f200_install
-mkdir ~/ivcam
-sh install.sh ~/ivcam
-cd ~/RealSense-ROS/realsense_dist/2.3
-./install_realsense_ros.sh
-
-
-  
-
-
+git clone https://github.com/IntelRealSense/librealsense.git
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install libusb-1.0-0-dev
+cd librealsense
+./scripts/install_glfw3.sh
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && udevadm trigger
+./scripts/patch-uvcvideo-ubuntu-mainline.sh
+sudo modprobe uvcvideo
+make && sudo make install
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
+cd ~/catkin_ws/src
+https://github.com/intel-ros/realsense.git
+cd realsense
+git checkout stable
+rosdep install --skip-keys=librealsense --from-paths -i . 
+cd ~/catkin_ws
+catkin_make
