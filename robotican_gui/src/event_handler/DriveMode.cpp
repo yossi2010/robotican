48740c19b3c15646fc0f2460a1d8289af57328d0
sub <elhayr1@gmail.com>
2016-08-23 12:17:20 +0300

    driving mode modification

--- robotican_gui/src/event_handler/DriveMode.cpp
+++ robotican_gui/src/event_handler/DriveMode.cpp
@@ -4,15 +4,18 @@
 
 #include "DriveMode.h"
 
-DriveMode::DriveMode() : _group("arm")
-{
-
+DriveMode::DriveMode() : _group("arm") {
+    _group.setPlannerId("SBLkConfigDefault");
+    _group.setPlanningTime(5.0);
+    _group.setNumPlanningAttempts(2);
 }
 
 bool DriveMode::moveArm()
 {
     //plan
     _group.setNamedTarget("Driving Mode");
+
+
     bool plan_success = _group.plan(_drivingModePlan);
 
     //execute
