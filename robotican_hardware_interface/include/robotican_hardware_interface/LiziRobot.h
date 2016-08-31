2fce5076463aaac1e227bf86c30bf7dc1a9bc7ba
sub <elhayr1@gmail.com>
2016-08-31 16:18:16 +0300

    adapt lizi to new ric

--- robotican_hardware_interface/include/robotican_hardware_interface/LiziRobot.h
+++ robotican_hardware_interface/include/robotican_hardware_interface/LiziRobot.h
@@ -8,35 +8,31 @@
 #include <ric_board/Motor.h>
 #include <robotican_hardware_interface/robot_base.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <robotican_hardware_interface/dynamixel_pro_controller.h>
+#include <dynamixel_msgs/JointState.h>
 
 namespace robotican_hardware {
+
+    typedef std::pair<std::string, JointInfo_t> joint_pair;
+
     class LiziRobot : public RobotBase {
     private:
-        std::pair<std::string, JointInfo_t> _rearLeftMotorJointInfo;
-        std::pair<std::string, JointInfo_t> _rearRightMotorJointInfo;
-        std::pair<std::string, JointInfo_t> _panInfo;
-        std::pair<std::string, JointInfo_t> _tiltInfo;
+        hardware_interface::PositionJointInterface _posJointInterface;
+        hardware_interface::JointStateInterface _jointStateInterface;
+        hardware_interface::VelocityJointInterface _velocityJointInterface;
+        dynamixel_pro_controller::DynamixelProController* _dynamixelProController;
 
-        ros::Publisher _rearLeftMotorCmd;
-        ros::Publisher _rearRightMotorCmd;
-        ros::Publisher _panCmd;
-        ros::Publisher _tiltCmd;
+        joint_pair _rearLeftMotorJointInfo;
+        joint_pair _rearRightMotorJointInfo;
+        joint_pair _frontLeftMotorJointInfo;
+        joint_pair _frontRightMotorJointInfo;
 
-        ros::Subscriber _rearLeftMotorState;
-        ros::Subscriber _rearRightMotorState;
-        ros::Subscriber _panState;
-        ros::Subscriber _tiltState;
+        joint_pair _panInfo;
+        joint_pair _tiltInfo;
 
-        void rearRightMotorStateCallback(const ric_board::Motor::ConstPtr &msg);
-
-        void rearLeftMotorStateCallback(const ric_board::Motor::ConstPtr &msg);
-
-        void panCallback(const std_msgs::Float32::ConstPtr &msg);
-
-        void tiltCallback(const std_msgs::Float32::ConstPtr &msg);
+        bool _havePanTilt;
 
     protected:
-        hardware_interface::PositionJointInterface _positionJointInterface;
 
     public:
         LiziRobot();
--- robotican_hardware_interface/src/robotican_hardware_interface/LiziRobot.cpp
+++ robotican_hardware_interface/src/robotican_hardware_interface/LiziRobot.cpp
@@ -6,78 +6,89 @@
 
 namespace robotican_hardware {
 
+    LiziRobot::LiziRobot()
+    {
+        std::string rearLeftJointName,
+                    rearRightJointName,
+                    frontRightJointName,
+                    frontLeftJointName,
+                    panJointName,
+                    tiltJointName;
 
-    void LiziRobot::rearRightMotorStateCallback(const ric_board::Motor::ConstPtr &msg) {
-        _rearRightMotorJointInfo.second.position = msg->position;
-        _rearRightMotorJointInfo.second.velocity = msg->velocity;
-    }
-
-    void LiziRobot::rearLeftMotorStateCallback(const ric_board::Motor::ConstPtr &msg) {
-        _rearLeftMotorJointInfo.second.position = msg->position;
-        _rearLeftMotorJointInfo.second.velocity = msg->velocity;
-    }
-
-    LiziRobot::LiziRobot() {
-        std::string rearLeftMotorPub, rearRightMotorPub, rearLeftMotorSub,
-                rearRightMotorSub, rearLeftJointName, rearRightJointName,
-                panPubTopic, panSubTopic, panJointName,
-                tiltPubTopic, tiltSubTopic, tiltJointName;
-
-        if(!_nodeHandle.getParam("rear_left_motor_pub", rearLeftMotorPub) ||
-           !_nodeHandle.getParam("rear_right_motor_pub", rearRightMotorPub) ||
-           !_nodeHandle.getParam("rear_left_motor_joint_name", rearLeftJointName) ||
+        if(!_nodeHandle.getParam("rear_left_motor_joint_name", rearLeftJointName) ||
            !_nodeHandle.getParam("rear_right_motor_joint_name", rearRightJointName) ||
-           !_nodeHandle.getParam("rear_right_motor_sub", rearRightMotorSub) ||
-           !_nodeHandle.getParam("rear_left_motor_sub", rearLeftMotorSub)) {    /* parameters that must be instalize for the robot to work*/
-
-            ros_utils::rosError("The robot operator need to provide parameters : 'left_motor_topic', 'right_motor_topic'"
-                                        ", 'left_motor_joint_name', 'right_motor_joint_name' to run the robot");
+            !_nodeHandle.getParam("front_right_motor_joint_name", frontLeftJointName) ||
+            !_nodeHandle.getParam("front_right_motor_joint_name", frontRightJointName))
+        {
+            ros_utils::rosError("You must specify the following parameters: rear_left_motor_joint_name,"
+                                                                            "rear_right_motor_joint_name"
+                                                                            "front_right_motor_joint_name,"
+                                                                            "front_right_motor_joint_name");
             ros::shutdown();
         }
 
-        _rearLeftMotorJointInfo = std::pair<std::string, JointInfo_t>(rearLeftJointName, JointInfo_t());
-        _rearRightMotorJointInfo = (std::pair<std::string, JointInfo_t>(rearRightJointName, JointInfo_t()));
-        _rearLeftMotorCmd = _nodeHandle.advertise<std_msgs::Float32>(rearLeftMotorPub, 10);
-        _rearRightMotorCmd = _nodeHandle.advertise<std_msgs::Float32>(rearRightMotorPub, 10);
-        _rearLeftMotorState = _nodeHandle.subscribe<ric_board::Motor>(rearLeftMotorSub, 10 ,&LiziRobot::rearLeftMotorStateCallback, this);
-        _rearRightMotorState = _nodeHandle.subscribe<ric_board::Motor>(rearRightMotorSub, 10 ,&LiziRobot::rearRightMotorStateCallback, this);
+        _rearLeftMotorJointInfo = joint_pair(rearLeftJointName, JointInfo_t());
+        _rearRightMotorJointInfo = joint_pair(rearRightJointName, JointInfo_t());
+        _frontRightMotorJointInfo = joint_pair(frontLeftJointName, JointInfo_t());
+        _frontRightMotorJointInfo = joint_pair(frontRightJointName, JointInfo_t());
 
-        hardware_interface::JointStateHandle leftJointStateHandle(_rearLeftMotorJointInfo.first,
+        hardware_interface::JointStateHandle rearLeftJointStateHandle(_rearLeftMotorJointInfo.first,
                                                                   &_rearLeftMotorJointInfo.second.position,
                                                                   &_rearLeftMotorJointInfo.second.velocity,
                                                                   &_rearLeftMotorJointInfo.second.effort);
-        hardware_interface::JointStateHandle rightJointStateHandle(_rearRightMotorJointInfo.first,
+        hardware_interface::JointStateHandle rearRightJointStateHandle(_rearRightMotorJointInfo.first,
                                                                    &_rearRightMotorJointInfo.second.position,
                                                                    &_rearRightMotorJointInfo.second.velocity,
                                                                    &_rearRightMotorJointInfo.second.effort);
+        hardware_interface::JointStateHandle frontLeftJointStateHandle(_frontLeftMotorJointInfo.first,
+                                                                  &_frontLeftMotorJointInfo.second.position,
+                                                                  &_frontLeftMotorJointInfo.second.velocity,
+                                                                  &_frontLeftMotorJointInfo.second.effort);
+        hardware_interface::JointStateHandle frontRightJointStateHandle(_frontRightMotorJointInfo.first,
+                                                                   &_frontRightMotorJointInfo.second.position,
+                                                                   &_frontRightMotorJointInfo.second.velocity,
+                                                                   &_frontRightMotorJointInfo.second.effort);
 
-        _jointStateInterface.registerHandle(leftJointStateHandle);
-        _jointStateInterface.registerHandle(rightJointStateHandle);
+        _jointStateInterface.registerHandle(rearLeftJointStateHandle);
+        _jointStateInterface.registerHandle(rearRightJointStateHandle);
+        _jointStateInterface.registerHandle(frontLeftJointStateHandle);
+        _jointStateInterface.registerHandle(frontRightJointStateHandle);
 
-        hardware_interface::JointHandle leftJointHandle(_jointStateInterface.getHandle(_rearLeftMotorJointInfo.first),
+        hardware_interface::JointHandle rearLeftJointHandle(_jointStateInterface.getHandle(_rearLeftMotorJointInfo.first),
                                                         &_rearLeftMotorJointInfo.second.cmd);
-        hardware_interface::JointHandle rightJointHandle(_jointStateInterface.getHandle(_rearRightMotorJointInfo.first),
+        hardware_interface::JointHandle rearRightJointHandle(_jointStateInterface.getHandle(_rearRightMotorJointInfo.first),
                                                          &_rearRightMotorJointInfo.second.cmd);
+        hardware_interface::JointHandle frontLeftJointHandle(_jointStateInterface.getHandle(_frontLeftMotorJointInfo.first),
+                                                            &_frontLeftMotorJointInfo.second.cmd);
+        hardware_interface::JointHandle frontRightJointHandle(_jointStateInterface.getHandle(_frontRightMotorJointInfo.first),
+                                                             &_frontRightMotorJointInfo.second.cmd);
 
-        _velocityJointInterface.registerHandle(leftJointHandle);
-        _velocityJointInterface.registerHandle(rightJointHandle);
+        _velocityJointInterface.registerHandle(rearLeftJointHandle);
+        _velocityJointInterface.registerHandle(rearRightJointHandle);
+        _velocityJointInterface.registerHandle(frontLeftJointHandle);
+        _velocityJointInterface.registerHandle(frontRightJointHandle);
 
 
-        if(_nodeHandle.getParam("pan_topic_pub", panPubTopic)
-           && _nodeHandle.getParam("pan_topic_sub", panSubTopic)
-           && _nodeHandle.getParam("pan_joint", panJointName)
-           && _nodeHandle.getParam("tilt_topic_pub", tiltPubTopic)
-           && _nodeHandle.getParam("tilt_topic_sub", tiltSubTopic)
-           && _nodeHandle.getParam("tilt_joint", tiltJointName)) {
 
-            _panCmd = _nodeHandle.advertise<std_msgs::Float32>(panPubTopic, 10);
-            _tiltCmd = _nodeHandle.advertise<std_msgs::Float32>(tiltPubTopic, 10);
+        _havePanTilt = false;
+        ros::param::param<bool>("have_pan_tilt", _havePanTilt, true);
+        if (_havePanTilt)
+        {
 
-            _panState = _nodeHandle.subscribe<std_msgs::Float32>(panSubTopic, 10, &LiziRobot::panCallback, this);
-            _tiltState = _nodeHandle.subscribe<std_msgs::Float32>(tiltSubTopic, 10, &LiziRobot::tiltCallback, this);
+            //////////////////////////////////////////
+            //
+           //        _jointStateInterface.registerHandle(rearLeftJointStateHandle);
+//
+            //hardware_interface::JointHandle rearLeftJointHandle(_jointStateInterface.getHandle(_rearLeftMotorJointInfo.first),
+            //&_rearLeftMotorJointInfo.second.cmd);
+            //       _velocityJointInterface.registerHandle(rearLeftJointHandle);
 
-            _panInfo = std::pair<std::string, JointInfo_t>(panJointName, JointInfo_t());
-            _tiltInfo = std::pair<std::string, JointInfo_t>(tiltJointName, JointInfo_t());
+            //////////////////////////////////////////
+            _panInfo = joint_pair(panJointName, JointInfo_t());
+            _tiltInfo = joint_pair(tiltJointName, JointInfo_t());
+
+            _dynamixelProController = new dynamixel_pro_controller::DynamixelProController(&_jointStateInterface, &_posJointInterface);
+            _dynamixelProController->startBroadcastingJointStates();
 
             hardware_interface::JointStateHandle panJointStateHandle(_panInfo.first,
                                                                      &_panInfo.second.position,
@@ -89,61 +100,29 @@ namespace robotican_hardware {
                                                                       &_tiltInfo.second.velocity,
                                                                       &_tiltInfo.second.effort );
 
-            _jointStateInterface.registerHandle(panJointStateHandle);
-            _jointStateInterface.registerHandle(tiltJointStateHandle);
-
-            hardware_interface::JointHandle panJointHandle(_jointStateInterface.getHandle(_panInfo.first), &_panInfo.second.cmd);
-            hardware_interface::JointHandle tiltJointHandle(_jointStateInterface.getHandle(_tiltInfo.first), &_tiltInfo.second.cmd);
-
-            _positionJointInterface.registerHandle(panJointHandle);
-            _positionJointInterface.registerHandle(tiltJointHandle);
-
         }
     }
 
     LiziRobot::~LiziRobot() {
-
+        if (_havePanTilt)
+            delete _dynamixelProController;
+        _dynamixelProController = NULL;
     }
 
     void LiziRobot::registerInterfaces() {
         RobotBase::registerInterfaces();
-        if(!_panInfo.first.empty() && _tiltInfo.first.empty()) {
-            registerInterface(&_positionJointInterface);
-        }
     }
 
     void LiziRobot::read() {
         RobotBase::read();
+        if(_havePanTilt)
+            _dynamixelProController->read();
     }
 
     void LiziRobot::write() {
         RobotBase::write();
 
-        std_msgs::Float32 leftMsg, rightMsg;
-
-        leftMsg.data = _rearLeftMotorJointInfo.second.cmd;
-        rightMsg.data = _rearRightMotorJointInfo.second.cmd;
-
-        _rearLeftMotorCmd.publish(leftMsg);
-        _rearRightMotorCmd.publish(rightMsg);
-
-        if(!_panInfo.first.empty() && _tiltInfo.first.empty()) {
-            std_msgs::Float32 panMsg, tiltMsg;
-
-            panMsg.data = _panInfo.second.cmd;
-            tiltMsg.data = _tiltInfo.second.cmd;
-
-            _panCmd.publish(panMsg);
-            _tiltCmd.publish(tiltMsg);
-        }
-
-    }
-
-    void LiziRobot::panCallback(const std_msgs::Float32::ConstPtr &msg) {
-        _panInfo.second.position = msg->data;
-    }
-
-    void LiziRobot::tiltCallback(const std_msgs::Float32::ConstPtr &msg) {
-        _tiltInfo.second.position = msg->data;
+        if(_havePanTilt)
+            _dynamixelProController->write();
     }
 }
