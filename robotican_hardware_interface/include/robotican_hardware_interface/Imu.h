5dc699f33306cd4f0929026696d2ea6d0e6ad963
Tom wiss <tom@robotican.net>
2016-09-05 13:50:43 +0300

    update

--- robotican_hardware_interface/include/robotican_hardware_interface/Imu.h
+++ robotican_hardware_interface/include/robotican_hardware_interface/Imu.h
@@ -6,6 +6,12 @@
 #define ROBOTICAN_HARDWARE_INTERFACE_IMU_H
 
 
+#define DEGs2RASs 0.0174532925
+
+#define G2ms 9.80665
+
+#define MILLI_GAUSS_2_TESLAS 1.0e-7
+
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/MagneticField.h>
 #include <robotican_hardware_interface/Device.h>
@@ -30,13 +36,23 @@ namespace robotican_hardware {
         bool _fuseCompass;
         bool _isStopClib;
 
+        double _imuLinearAccFix[3][3];
+        double _imuAngularVelocityFix[3][3];
+        double _imuMagnetometerFix[3][3];
+        double _imuRotationFix[3][3];
+        double _imuRotationOffset[3][3];
+
         bool _isStateChange;
         bool onSetImuClib(robotican_hardware_interface::setImuClib::Request &request, robotican_hardware_interface::setImuClib::Response &response);
+
+        void initArrays();
 
     public:
 
         Imu(byte id, TransportLayer *transportLayer, uint16_t fusionHz, std::string frameId, bool enableGyro,
-                    bool fuseCompass);
+                    bool fuseCompass, std::vector<double> imuLinearAccFix, std::vector<double> imuAngularVelocityFix,
+                    std::vector<double> imuMagnetometerFix, std::vector<double> imuRotationFix,
+                    std::vector<double> imuRotationOffset);
 
         virtual void update(const DeviceMessage *deviceMessage);
 
