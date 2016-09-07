2de859157afd18091f9e2917cc386fe1ea152334
Yam Geva <yam@robotican.net>
2016-09-07 12:00:44 +0300

    pnp

--- robotican_demos/src/pick_and_place.cpp
+++ robotican_demos/src/pick_and_place.cpp
@@ -125,7 +125,8 @@ void look_down() {
 
 geometry_msgs::PoseStamped pre_grasp_pose(geometry_msgs::PoseStamped object){
     geometry_msgs::PoseStamped target_pose=object;
-
+    target_pose.z+=0.1;
+/*
 
     tf::Vector3 v;
     v.setX(object.pose.position.x);
@@ -143,7 +144,7 @@ geometry_msgs::PoseStamped pre_grasp_pose(geometry_msgs::PoseStamped object){
     target_pose.pose.position.z +=0;
     target_pose.pose.orientation= tf::createQuaternionMsgFromRollPitchYaw(0,0,pick_yaw );
     pick_pub.publish(target_pose);
-
+*/
     return target_pose;
 
 }
