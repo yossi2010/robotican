53ac1803a31a9493bea3a758545aa80f328c1b55
sub <elhayr1@gmail.com>
2016-09-08 14:28:21 +0300

    slip factor

--- robotican_hardware_interface/include/robotican_hardware_interface/LiziRobot.h
+++ robotican_hardware_interface/include/robotican_hardware_interface/LiziRobot.h
@@ -13,29 +13,29 @@
 
 namespace robotican_hardware {
 
-    typedef std::pair<std::string, JointInfo_t> joint_pair;
+    //typedef std::pair<std::string, JointInfo_t> joint_pair;
 
     class LiziRobot : public RobotBase {
     private:
-        hardware_interface::PositionJointInterface _posJointInterface;
+   //     hardware_interface::PositionJointInterface _posJointInterface;
 
-        joint_pair _panInfo;
-        joint_pair _tiltInfo;
-        ros::Publisher _panPub;
-        ros::Publisher _tiltPub;
-        ros::Subscriber _panSub;
-        ros::Subscriber _tiltSub;
-        std::string _panSubTopic,
-                    _tiltSubTopic,
-                    _panPubTopic,
-                    _tiltPubTopic;
+//        joint_pair _panInfo;
+//        joint_pair _tiltInfo;
+//        ros::Publisher _panPub;
+//        ros::Publisher _tiltPub;
+//        ros::Subscriber _panSub;
+//        ros::Subscriber _tiltSub;
+//        std::string _panSubTopic,
+//                    _tiltSubTopic,
+//                    _panPubTopic,
+//                    _tiltPubTopic;
 
-        bool _havePanTilt;
-        void preparePanTilt();
-        void buildConnections();
-
-        void panCallBack(const dynamixel_msgs::JointState::ConstPtr &msg);
-        void tiltCallBack(const dynamixel_msgs::JointState::ConstPtr &msg);
+//        bool _havePanTilt;
+//        void preparePanTilt();
+//        void buildConnections();
+//
+//        void panCallBack(const dynamixel_msgs::JointState::ConstPtr &msg);
+//        void tiltCallBack(const dynamixel_msgs::JointState::ConstPtr &msg);
 
     protected:
 
--- robotican_hardware_interface/src/robotican_hardware_interface/LiziRobot.cpp
+++ robotican_hardware_interface/src/robotican_hardware_interface/LiziRobot.cpp
@@ -8,14 +8,14 @@ namespace robotican_hardware {
 
     LiziRobot::LiziRobot()
     {
-        _havePanTilt = false;
-        ros::param::param<bool>("have_pan_tilt", _havePanTilt, true);
+        //_havePanTilt = false;
+        //ros::param::param<bool>("have_pan_tilt", _havePanTilt, true);
 
-        if (_havePanTilt)
-        {
-            preparePanTilt();
-            buildConnections();
-        }
+//        if (_havePanTilt)
+//        {
+//            preparePanTilt();
+//            buildConnections();
+//        }
     } //delsdfld
 
     LiziRobot::~LiziRobot() {
@@ -32,70 +32,70 @@ namespace robotican_hardware {
 
     void LiziRobot::write() {
         RobotBase::write();
-        std_msgs::Float64 panMsg, tiltMsg;
-
-        panMsg.data = _panInfo.second.cmd;
-        tiltMsg.data = _tiltInfo.second.cmd;
-        _panPub.publish(panMsg);
-        _tiltPub.publish(tiltMsg);
+//        std_msgs::Float64 panMsg, tiltMsg;
+//
+//        panMsg.data = _panInfo.second.cmd;
+//        tiltMsg.data = _tiltInfo.second.cmd;
+//        _panPub.publish(panMsg);
+//        _tiltPub.publish(tiltMsg);
     }
 
-    void LiziRobot::preparePanTilt()
-    {
-        std::string panJointName,
-                    tiltJointName;
+//    void LiziRobot::preparePanTilt()
+//    {
+//        std::string panJointName,
+//                    tiltJointName;
+//
+//        _nodeHandle.getParam("pan_joint", panJointName);
+//        _nodeHandle.getParam("tilt_joint", tiltJointName);
+//
+//        _panInfo = joint_pair(panJointName, JointInfo_t());
+//        _tiltInfo = joint_pair(tiltJointName, JointInfo_t());
+//
+//        hardware_interface::JointStateHandle panJointStateHandle(_panInfo.first,
+//                                                                 &_panInfo.second.position,
+//                                                                 &_panInfo.second.velocity,
+//                                                                 &_panInfo.second.effort );
+//
+//        hardware_interface::JointStateHandle tiltJointStateHandle(_tiltInfo.first,
+//                                                                  &_tiltInfo.second.position,
+//                                                                  &_tiltInfo.second.velocity,
+//                                                                  &_tiltInfo.second.effort );
+//
+//        _jointStateInterface.registerHandle(panJointStateHandle);
+//        _jointStateInterface.registerHandle(tiltJointStateHandle);
+//
+//        hardware_interface::JointHandle panJointHandle(_jointStateInterface.getHandle(_panInfo.first),
+//                                                       &_panInfo.second.cmd);
+//        hardware_interface::JointHandle tiltJointHandle(_jointStateInterface.getHandle(_tiltInfo.first),
+//                                                        &_tiltInfo.second.cmd);
+//
+//        _posJointInterface.registerHandle(panJointHandle);
+//        _posJointInterface.registerHandle(tiltJointHandle);
+//    }
 
-        _nodeHandle.getParam("pan_joint", panJointName);
-        _nodeHandle.getParam("tilt_joint", tiltJointName);
+//    void LiziRobot::buildConnections()
+//    {
+//        _nodeHandle.getParam("pan_topic_sub", _panSubTopic);
+//        _nodeHandle.getParam("tilt_topic_sub", _tiltSubTopic);
+//        _nodeHandle.getParam("pan_topic_pub", _panPubTopic);
+//        _nodeHandle.getParam("tilt_topic_pub", _tiltPubTopic);
+//        _panPub = _nodeHandle.advertise<std_msgs::Float64>(_panPubTopic, 10);
+//        _tiltPub = _nodeHandle.advertise<std_msgs::Float64>(_tiltPubTopic, 10);
+//        _panSub = _nodeHandle.subscribe<dynamixel_msgs::JointState>(_panSubTopic, 10, &LiziRobot::panCallBack, this);
+//        _tiltSub = _nodeHandle.subscribe<dynamixel_msgs::JointState>(_tiltSubTopic, 10, &LiziRobot::tiltCallBack, this);
+//    }
 
-        _panInfo = joint_pair(panJointName, JointInfo_t());
-        _tiltInfo = joint_pair(tiltJointName, JointInfo_t());
-
-        hardware_interface::JointStateHandle panJointStateHandle(_panInfo.first,
-                                                                 &_panInfo.second.position,
-                                                                 &_panInfo.second.velocity,
-                                                                 &_panInfo.second.effort );
-
-        hardware_interface::JointStateHandle tiltJointStateHandle(_tiltInfo.first,
-                                                                  &_tiltInfo.second.position,
-                                                                  &_tiltInfo.second.velocity,
-                                                                  &_tiltInfo.second.effort );
-
-        _jointStateInterface.registerHandle(panJointStateHandle);
-        _jointStateInterface.registerHandle(tiltJointStateHandle);
-
-        hardware_interface::JointHandle panJointHandle(_jointStateInterface.getHandle(_panInfo.first),
-                                                       &_panInfo.second.cmd);
-        hardware_interface::JointHandle tiltJointHandle(_jointStateInterface.getHandle(_tiltInfo.first),
-                                                        &_tiltInfo.second.cmd);
-
-        _posJointInterface.registerHandle(panJointHandle);
-        _posJointInterface.registerHandle(tiltJointHandle);
+//    void LiziRobot::panCallBack(const dynamixel_msgs::JointState::ConstPtr &msg)
+//    {
+//        _panInfo.second.position = msg->current_pos;
+//        _panInfo.second.velocity = msg->velocity;
+//        _panInfo.second.effort = msg->load;
+//    }
+//
+//    void LiziRobot::tiltCallBack(const dynamixel_msgs::JointState::ConstPtr &msg)
+//    {
+//        _tiltInfo.second.position = msg->current_pos;
+//        _tiltInfo.second.velocity = msg->velocity;
+//        _tiltInfo.second.effort = msg->load;
+//    }
     }
-
-    void LiziRobot::buildConnections()
-    {
-        _nodeHandle.getParam("pan_topic_sub", _panSubTopic);
-        _nodeHandle.getParam("tilt_topic_sub", _tiltSubTopic);
-        _nodeHandle.getParam("pan_topic_pub", _panPubTopic);
-        _nodeHandle.getParam("tilt_topic_pub", _tiltPubTopic);
-        _panPub = _nodeHandle.advertise<std_msgs::Float64>(_panPubTopic, 10);
-        _tiltPub = _nodeHandle.advertise<std_msgs::Float64>(_tiltPubTopic, 10);
-        _panSub = _nodeHandle.subscribe<dynamixel_msgs::JointState>(_panSubTopic, 10, &LiziRobot::panCallBack, this);
-        _tiltSub = _nodeHandle.subscribe<dynamixel_msgs::JointState>(_tiltSubTopic, 10, &LiziRobot::tiltCallBack, this);
-    }
-
-    void LiziRobot::panCallBack(const dynamixel_msgs::JointState::ConstPtr &msg)
-    {
-        _panInfo.second.position = msg->current_pos;
-        _panInfo.second.velocity = msg->velocity;
-        _panInfo.second.effort = msg->load;
-    }
-
-    void LiziRobot::tiltCallBack(const dynamixel_msgs::JointState::ConstPtr &msg)
-    {
-        _tiltInfo.second.position = msg->current_pos;
-        _tiltInfo.second.velocity = msg->velocity;
-        _tiltInfo.second.effort = msg->load;
-    }
-}
