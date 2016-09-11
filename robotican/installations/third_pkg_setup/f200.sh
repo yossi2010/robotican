9841db55ea9406119634abea3794ec9507a488b8
Tom wiss <tom@robotican.net>
2016-09-11 09:41:04 +0300

    update

--- robotican/installations/third_pkg_setup/f200.sh
+++ robotican/installations/third_pkg_setup/f200.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-cd ~
-git clone https://github.com/IntelRealSense/librealsense.git
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install libusb-1.0-0-dev
-cd librealsense
-./scripts/install_glfw3.sh
-sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules && udevadm trigger
-./scripts/patch-uvcvideo-ubuntu-mainline.sh
-sudo modprobe uvcvideo
-make && sudo make install
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
-cd ~/catkin_ws/src
-git clone https://github.com/intel-ros/realsense.git
-cd realsense
-git checkout stable
-rosdep install --skip-keys=librealsense --from-paths -i . 
-cd ~/catkin_ws
-catkin_make
+# cd ~
+# git clone https://github.com/IntelRealSense/librealsense.git
+# sudo apt-get update && sudo apt-get upgrade
+# sudo apt-get install libusb-1.0-0-dev
+# cd librealsense
+# ./scripts/install_glfw3.sh
+# sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
+# sudo udevadm control --reload-rules && udevadm trigger
+# ./scripts/patch-uvcvideo-ubuntu-mainline.sh
+# sudo modprobe uvcvideo
+# make && sudo make install
+# echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
+# cd ~/catkin_ws/src
+# git clone https://github.com/intel-ros/realsense.git
+# cd realsense
+# git checkout stable
+# rosdep install --skip-keys=librealsense --from-paths -i . 
+# cd ~/catkin_ws
+# catkin_make
+
+sudo apt-get -y install ros-indigo-realsense-camera ros-indigo-librealsense
