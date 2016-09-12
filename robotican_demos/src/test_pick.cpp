a9b2d1c5203ae863cfcf665825184b83a2a41a13
Yam Geva <yam@robotican.net>
2016-09-12 15:49:08 +0300

    test pick

--- robotican_demos/src/test_pick.cpp
+++ robotican_demos/src/test_pick.cpp
@@ -33,7 +33,7 @@ void look_down() {
     traj.points[0].time_from_start = ros::Duration(1.0);
     std::vector<double> q_goal(2);
     q_goal[0]=0.0;
-    q_goal[1]=0.4;
+    q_goal[1]=0.6;
     traj.points[0].positions=q_goal;
     traj.points[0].velocities.push_back(0);
     traj.points[0].velocities.push_back(0);
@@ -42,51 +42,60 @@ void look_down() {
 
 void demoPick(moveit::planning_interface::MoveGroup &group)
 {
- std::vector<moveit_msgs::Grasp> grasps;
-  for (std::size_t i = 0 ; i < 1000 ; ++i)
-  {
-    geometry_msgs::PoseStamped p = group.getRandomPose();
-    p.pose.orientation.x = 0;
-   p.pose.orientation.y = 0;
-    p.pose.orientation.z = 0;
-    p.pose.orientation.w = 1;
+ //std::vector<moveit_msgs::Grasp> grasps;
+//  for (std::size_t i = 0 ; i < 1 ; ++i)
+ // {
+
     moveit_msgs::Grasp g;
-   g.grasp_pose = p;
-  //  g.grasp_pose.pose.orientation.w=1;
-  // p.header.frame_id="gripper_link";
+    g.grasp_posture.joint_names.push_back("left_finger_joint");
+    g.grasp_posture.joint_names.push_back("right_finger_joint");
+
+   g.grasp_pose.pose.orientation.w=1;
+  /* g.grasp_pose = p;
+
+
 
     g.pre_grasp_approach.direction.vector.x = 1.0;
-
-    g.pre_grasp_approach.min_distance = 0.02;
-    g.pre_grasp_approach.desired_distance = 0.05;
+ g.pre_grasp_approach.direction.header.frame_id="gripper_link";
+  g.pre_grasp_approach.direction.header.stamp=ros::Time::now();
+    g.pre_grasp_approach.min_distance = 0.01;
+    g.pre_grasp_approach.desired_distance = 0.06;
 
     g.post_grasp_retreat.direction.vector.z = 1.0;
-    g.post_grasp_retreat.direction.header = p.header;
+    g.post_grasp_retreat.direction.vector.x = -1.0;
+
+   g.post_grasp_retreat.direction.header.stamp = ros::Time::now();
+    g.post_grasp_retreat.direction.header.frame_id="base_footprint";
     g.post_grasp_retreat.min_distance = 0.1;
-    g.post_grasp_retreat.desired_distance = 0.2;
+    g.post_grasp_retreat.desired_distance = 0.1;
 
-
+ //g.pre_grasp_posture.header.frame_id="gripper_link";
     g.pre_grasp_posture.joint_names.push_back("left_finger_joint");
     g.pre_grasp_posture.joint_names.push_back("right_finger_joint");
     g.pre_grasp_posture.points.resize(1);
     g.pre_grasp_posture.points[0].positions.resize(2);
     g.pre_grasp_posture.points[0].positions[0] = -0.57;
     g.pre_grasp_posture.points[0].positions[1] = 0.57;
+    g.pre_grasp_posture.points[0].time_from_start = ros::Duration(2.0);
 
+    // g.grasp_posture.header.frame_id="gripper_link";
     g.grasp_posture.joint_names.push_back("left_finger_joint");
     g.grasp_posture.joint_names.push_back("right_finger_joint");
     g.grasp_posture.points.resize(1);
     g.grasp_posture.points[0].positions.resize(2);
     g.grasp_posture.points[0].positions[0] = 0.3;
     g.grasp_posture.points[0].positions[1] = -0.3;
+     g.grasp_posture.points[0].time_from_start = ros::Duration(2.0);
 
     g.allowed_touch_objects.push_back("wrist_link");
     g.allowed_touch_objects.push_back("left_finger_link");
     g.allowed_touch_objects.push_back("right_finger_link");
 
-   grasps.push_back(g);
-  }
-  group.pick("can");
+*/
+//std::cout <<g <<std::endl;
+  // grasps.push_back(g);
+ // }
+  group.pick("can",g);
 }
 
 int main(int argc, char **argv) {
@@ -105,13 +114,12 @@ int main(int argc, char **argv) {
     moveit::planning_interface::MoveGroup group("arm");
 
 
-    group.setMaxVelocityScalingFactor(0.1);
+    group.setMaxVelocityScalingFactor(0.01);
     group.setMaxAccelerationScalingFactor(0.5);
     group.setPlanningTime(5.0);
     group.setNumPlanningAttempts(10);
     group.setPlannerId("RRTConnectkConfigDefault");
     group.setPoseReferenceFrame("base_footprint");
-
 
 
     spinner.start();
@@ -120,7 +128,7 @@ int main(int argc, char **argv) {
     ROS_INFO("Waiting for update_collision service...");
     uc_client.waitForExistence();
     uc_client_ptr = &uc_client;
-    set_collision_update(true);
+    set_collision_update(false);
     ROS_INFO("Looking down...");
     look_down();
 
@@ -150,5 +158,4 @@ bool set_collision_update(bool state){
     }
 
 }
-
 
