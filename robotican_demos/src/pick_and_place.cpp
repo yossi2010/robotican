af87e49bb412505ba011ea3c50b4a07b07c3d4c4
Tom wiss <tom@robotican.net>
2016-09-14 17:24:27 +0300

    update fix param

--- robotican_demos/src/pick_and_place.cpp
+++ robotican_demos/src/pick_and_place.cpp
@@ -42,9 +42,11 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
     ros::NodeHandle pn("~");
     std::string startPositionName;
+    std::string pickAndPlaceObjName;
     std::string object_name;
     pn.param<std::string>("start_position_name", startPositionName, "pre_grasp2");
     pn.param<std::string>("object_name", object_name, "object");
+    pn.param<std::string>("pick_place_object", pickAndPlaceObjName, "can");
 
     ROS_INFO("Hello");
     moveit::planning_interface::MoveGroup group("arm");
@@ -64,7 +66,7 @@ int main(int argc, char **argv) {
         pub_controller_command = n.advertise<trajectory_msgs::JointTrajectory>("/pan_tilt_trajectory_controller/command", 2);
         ROS_INFO("Waiting for the moveit action server to come up");
 
-        std::string uc="/update_collision/"+object_name;
+        std::string uc="/update_collision/" + object_name;
         ros::ServiceClient uc_client = n.serviceClient<std_srvs::SetBool>(uc);
         ROS_INFO("Waiting for update_collision service...");
         uc_client.waitForExistence();
@@ -80,15 +82,14 @@ int main(int argc, char **argv) {
 
         PickClient pickClient("pickup", true);
         pickClient.waitForServer();
-        std::string objectName = "can";
-        moveit_msgs::PickupGoal pickGoal = BuildPickGoal(objectName);
+
+        moveit_msgs::PickupGoal pickGoal = BuildPickGoal(pickAndPlaceObjName);
         pickClient.sendGoalAndWait(pickGoal);
 
         PlaceClient placeClient("place", true);
         placeClient.waitForServer();
 
-        moveit_msgs::PlaceGoal placeGoal = buildPlaceGoal(objectName);
-
+        moveit_msgs::PlaceGoal placeGoal = buildPlaceGoal(pickAndPlaceObjName);
         placeClient.sendGoalAndWait(placeGoal);
 
     }
