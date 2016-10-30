3ad46e3c5ead49cf4ecc7b48bb31feecf0b6c33e
Tom wiss <tom@robotican.net>
2016-10-30 09:22:30 +0200

    update

---
+++ robotican_hardware_interface/include/robotican_hardware_interface/dynamixel_controller.h
@@ -0,0 +1,18 @@
+//
+// Created by tom on 30/10/16.
+//
+
+#ifndef ROBOTICAN_HARDWARE_INTERFACE_DYNAMIXEL_CONTROLLER_H
+#define ROBOTICAN_HARDWARE_INTERFACE_DYNAMIXEL_CONTROLLER_H
+
+#include <ros/ros.h>
+#include <robotican_hardware_interface/dynamixel_driver.h>
+
+namespace dynamixel_controller {
+    class DynamixelController {
+    private:
+
+    };
+}
+
+#endif //ROBOTICAN_HARDWARE_INTERFACE_DYNAMIXEL_CONTROLLER_H
---
+++ robotican_hardware_interface/src/robotican_hardware_interface/dynamixel_controller.cpp
@@ -0,0 +1,9 @@
+//
+// Created by tom on 30/10/16.
+//
+
+#include "robotican_hardware_interface/dynamixel_controller.h"
+
+namespace dynamixel_controller {
+
+}
