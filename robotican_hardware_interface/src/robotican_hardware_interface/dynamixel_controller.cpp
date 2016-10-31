6d6c7f7f7f33c69628d19638b712da104f440cb8
Tom wiss <tom@robotican.net>
2016-10-31 12:33:05 +0200

    update

--- robotican_hardware_interface/src/robotican_hardware_interface/dynamixel_controller.cpp
+++ robotican_hardware_interface/src/robotican_hardware_interface/dynamixel_controller.cpp
@@ -178,7 +178,7 @@ namespace dynamixel_controller {
     }
 
     double DynamixelController::posToRads(uint32_t ticks, const dynamixel_info &info) {
-        ROS_INFO("%d", ticks);
+        ROS_INFO("%u", ticks);
         double cprDev2 = info.cpr / 2.0f;
         return (ticks - cprDev2) * M_PI / cprDev2;
     }
